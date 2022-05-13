988a8cfadc14c13e492a8684f761cc3b6107cd40
Ayo Daramola <103411493+dultimategithub@users.noreply.github.com>
2022-05-13 14:46:13 +0100

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,32 @@
+// GRADING RESULT SYSTEM 
+// Step 1: Take a number
+// Step 2: if the mark is between 70 and 100, Print 'A'
+// Step 3: if the mark is between 60 and 69, Print 'B'
+// Step 4: if the mark is between 50 and 59, Print 'C'
+// Step 5: if the mark is between 45 and 49, Print 'D'
+// Step 6: if the mark is between 40 and 44, Print 'E'
+// Step 7: if the mark is between 0 and 39, Print 'F'
+// Step 8: Result not found 
+
+let score = ;
+if(score >= 70 && score <= 100){
+    console.log('A');
+}
+else if(score >= 60 && score <= 69){
+    console.log('B');
+}
+else if(score >= 50 && score <= 59){
+    console.log('C');
+}
+else if(score >= 45 && score <= 59){
+    console.log('D');
+}
+else if(score >= 40 && score <= 44){
+    console.log('E');
+}
+else if(score >= 0 && score <= 39){
+    console.log('F');
+}
+else{
+    console.log('Result not found');
+}
